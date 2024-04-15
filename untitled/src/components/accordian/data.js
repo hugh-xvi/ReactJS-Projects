bad4c8953443e36392a68f09dd24fd4467fa6a71
Hugh <imaingodyrlol@gmail.com>
2024-04-14 23:22:11 -0700

    accordian

---
+++ untitled/src/components/accordian/data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id: '1',
+        question: "What",
+        answer:"Yes"
+    },
+    {
+        id: '2',
+        question: "How",
+        answer:"No"
+    },
+    {
+        id: '3',
+        question: "When",
+        answer:"Maybe"
+    },
+    {
+        id: '4',
+        question: "Why",
+        answer:"So"
+    },
+];
+export default data;
